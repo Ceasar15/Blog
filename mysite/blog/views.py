a78e822cdc7b135ab7bc27e4669ba4b1b38e185b
Ceasar15 <ceasarkwadwo@gmail.com>
2020-11-23 16:20:20 +0000

    implemented tag list and its view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-
+from taggit.models import Tag
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
+
+
+
 # Create your views here.
 
 
@@ -17,27 +20,42 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 1
 
+
+
 # Function based Post list View
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list,1) # 3 post per each page
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     context = {
-#         'page': page,
-#         'posts': posts,
-#     }
-#     return render(request, 'blog/post/list.html', context)
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+
+
+    paginator = Paginator(object_list,1) # 3 post per each page
+    page = request.GET.get('page')
+
+
+
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    context = {
+        'page': page,
+        'posts': posts,
+        'tag': tag,
+    }
+    return render(request, 'blog/post/list.html', context)
 
 
 # Share post with emails
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
@@ -54,6 +72,7 @@ def post_share(request, post_id):
     context = {
         'post':post,
         'form': form,
+        'sent': sent,
     }
     return render(request, 'blog/post/share.html', context)
 
