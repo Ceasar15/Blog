755854fd2b0fa3a536dfbea3f68a2b77e41c0a02
Ceasar15 <ceasarkwadwo@gmail.com>
2020-11-25 15:01:47 +0000

    wrote my custom templatetag

---
+++ mysite/blog/templatetags/blog_tags.py
@@ -0,0 +1,13 @@
+from django import template
+from ..models import Post
+
+
+register = template.Library()
+
+@register.simple_tag
+def total_post():
+    return Post.published.count()
+
+
+
+
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from taggit.models import Tag
 
+
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
